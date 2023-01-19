033dfa9bfee1bab61b09342f353de25f32d6d0cc
Кирил Эктоф <kirill.ektoff@yandex.ru>
2023-01-19 19:22:03 +0700

    Initial commit. Added Player model with load method

---
+++ TerrariaConstructor/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ReactiveUI;
+using TerrariaConstructor.Models;
+
+namespace TerrariaConstructor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Player player = new Player();
+            player.LoadPlayer(@"C:\Users\Bellatrix\Documents\My Games\Terraria\Players\Пёплик.plr");
+        }
+    }
+}
---
+++ TerrariaConstructor/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace TerrariaConstructor.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    public byte Prefix { get; set; }
+    public int Stack { get; set; }
+    public bool IsFavorite { get; set; }
+}
